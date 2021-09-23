e70f4bf969497050f9b9fca45814bcf43c97ef3e
EunsilGil <eunsil.develop@gmail.com>
2021-09-23 15:23:42 +0900

    1차 source code complete

--- 1차_SolutionCode/1차 2급 2_solution_code.py
+++ 1차_SolutionCode/1차 2급 2_solution_code.py
@@ -1,4 +1,4 @@
-def solution(price, grade):
+def solution(price, grade):     # 함수 만들기
     answer = 0
 
     if grade == "S":
@@ -9,3 +9,15 @@ def solution(price, grade):
         answer = int(price*0.85)
     
     return answer
+
+price1 = 2500
+grade1 = "V"
+ret1 = solution(price1, grade1)
+
+print("solution 함수의 반환 값은", ret1, "입니다.")
+    
+price2 = 96900
+grade2 = "S"
+ret2 = solution(price2, grade2)
+
+print("solution 함수의 반환 값은", ret2, "입니다.")
